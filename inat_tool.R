572412302b1c7eb4bfc691c3053ba9aa2a41b8db
Zach Newkirk <sharknewkirk@msn.com>
2024-04-01 22:59:49 +1000

    Upgraded tool

--- inat_tool.R
+++ inat_tool.R
@@ -2,18 +2,28 @@
 library(tidyverse)
 
 #### Defining Functions ####
+# Month Selection
+month <- function(input){
+  months <- c("January", "February", "March", "April", "May", "June", 
+              "July", "August", "September", "October", "November", "December")
+  sprintf("%02d", match(input, months))
+}
 
+# Time Zone Selection
+time_zone <- function(time, location) {
+  time <- as.POSIXct(time, tz = "UTC")  
+  attr(time, "tzone") <- location       
+  return(time)
+}
 
 #### Importing Data ####
-animal_data <- read_csv("PATH") %>% # Change path to location of data file
-  select(1,4) %>% 
+animal_data <- read_csv("Wombat data.csv") %>% # INPUT DATA PATH HERE
+  select(1,4,23,24,35:39) %>%
   na.omit() %>%
-  separate(time_observed_at, into = c("date", "time", "time_zone"), sep = " ") %>%
-  select(-time_zone) %>%
-  mutate(hour = substr(time, 1, 2)) %>%
-  filter(substr(date, 6, 7) == "06") %>% #06 for June
-  mutate(hour = as.numeric(hour) + 9) %>% # 9 for adelaide time
-  mutate(hour = ifelse(hour >= 24, hour - 24, hour))
+  mutate(time_observed_at = time_zone(time_observed_at, "Australia/Adelaide")) %>% # INPUT TIME ZONE HERE
+  mutate(hour = substr(time_observed_at, 12, 13)) %>%
+  filter(substr(time_observed_at, 6, 7) == month("June")) # INPUT MONTH HERE
+
 
 #### Plotting ####
 ggplot(animal_data, aes(x=hour)) +
